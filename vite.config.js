d5c6c0d924ab39d5f675f798084325d00b40ae94
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2025-09-25 11:40:02 +0530

    Setting Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TODO_LIST-REACT/',
+  build: {
+    outDir: 'dist'
+  }
 })
